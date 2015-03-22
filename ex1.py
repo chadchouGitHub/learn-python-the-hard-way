74271c2ab7fb506ad0718cc9a5a08ae9228d5cbb
chadchouGitHub <chiachingchou@gmail.com>
2015-03-22 11:51:28 -0400

    Learn python the hard way
    The first ex1 code.

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print "Hello Word!"
+print "Hello Again"
+print "I Like typing thing."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
